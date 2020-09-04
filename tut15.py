4d4487b0936667afe48f0ebdab27a7e700676d62
Ayman Shabana <53878617+AymanShabana@users.noreply.github.com>
2020-09-04 21:41:05 +0200

    NLP week3

--- tut15.py
+++ tut15.py
@@ -22,13 +22,131 @@ corpus = []
 
 num_sentences = 0
 
-with open("/tmp/training_cleaned.csv") as csvfile:
+with open("tmp2/training_cleaned.csv") as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
       # Your Code here. Create list items where the first item is the text, found in row[5], and the second is the label. Note that the label is a '0' or a '4' in the text. When it's the former, make
       # your label to be 0, otherwise 1. Keep a count of the number of sentences in num_sentences
         list_item=[]
         # YOUR CODE HERE
-
+        list_item.append(row[5])
+        this_label = row[0]
+        if this_label == '0':
+            list_item.append(0)
+        else:
+            list_item.append(1)
         num_sentences = num_sentences + 1
         corpus.append(list_item)
+
+print(num_sentences)
+print(len(corpus))
+print(corpus[1])
+
+# Expected Output:
+# 1600000
+# 1600000
+# ["is upset that he can't update his Facebook by texting it... and might cry as a result  School today also. Blah!", 0]
+sentences=[]
+labels=[]
+random.shuffle(corpus)
+for x in range(training_size):
+    sentences.append(corpus[x][0])
+    labels.append(corpus[x][1])
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(sentences)
+
+word_index = tokenizer.word_index
+vocab_size=len(word_index)
+
+sequences = tokenizer.texts_to_sequences(sentences)
+padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+split = int(test_portion * training_size)
+
+test_sequences = padded[0:split]
+training_sequences = padded[split:training_size]
+test_labels = labels[0:split]
+training_labels = labels[split:training_size]
+
+print(vocab_size)
+print(word_index['i'])
+
+embeddings_index = {};
+with open('tmp2/glove.6B.100d.txt') as f:
+    for line in f:
+        values = line.split();
+        word = values[0];
+        coefs = np.asarray(values[1:], dtype='float32');
+        embeddings_index[word] = coefs;
+
+embeddings_matrix = np.zeros((vocab_size+1, embedding_dim));
+for word, i in word_index.items():
+    embedding_vector = embeddings_index.get(word);
+    if embedding_vector is not None:
+        embeddings_matrix[i] = embedding_vector;
+print(len(embeddings_matrix))
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size+1, embedding_dim, input_length=max_length, weights=[embeddings_matrix], trainable=False),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True)),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+model.summary()
+
+num_epochs = 50
+
+training_padded = np.array(training_sequences)
+training_labels = np.array(training_labels)
+testing_padded = np.array(test_sequences)
+testing_labels = np.array(test_labels)
+
+history = model.fit(training_padded, training_labels, epochs=num_epochs, validation_data=(testing_padded, testing_labels), verbose=2)
+
+print("Training Complete")
+
+import matplotlib.image  as mpimg
+import matplotlib.pyplot as plt
+
+#-----------------------------------------------------------
+# Retrieve a list of list results on training and test data
+# sets for each training epoch
+#-----------------------------------------------------------
+acc=history.history['accuracy']
+val_acc=history.history['val_accuracy']
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+
+epochs=range(len(acc)) # Get number of epochs
+
+#------------------------------------------------
+# Plot training and validation accuracy per epoch
+#------------------------------------------------
+plt.plot(epochs, acc, 'r')
+plt.plot(epochs, val_acc, 'b')
+plt.title('Training and validation accuracy')
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.legend(["Accuracy", "Validation Accuracy"])
+
+plt.figure()
+
+#------------------------------------------------
+# Plot training and validation loss per epoch
+#------------------------------------------------
+plt.plot(epochs, loss, 'r')
+plt.plot(epochs, val_loss, 'b')
+plt.title('Training and validation loss')
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend(["Loss", "Validation Loss"])
+
+plt.figure()
+
+
+# Expected Output
+# A chart where the validation loss does not increase sharply!
